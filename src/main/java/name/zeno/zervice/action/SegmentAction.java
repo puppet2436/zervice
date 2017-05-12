262eb886021f720d81b27f6e17793adc91e2e658
陈治谋 <513500085@qq.com>
2017-05-12 13:50:17 +0800

    - 分词兼容特殊字符串

--- src/main/java/name/zeno/zervice/action/SegmentAction.java
+++ src/main/java/name/zeno/zervice/action/SegmentAction.java
@@ -3,19 +3,35 @@ package name.zeno.zervice.action;
 import com.huaban.analysis.jieba.JiebaSegmenter;
 import com.huaban.analysis.jieba.SegToken;
 import name.zeno.zervice.model.ApiResult;
+import org.apache.log4j.Logger;
+import org.nutz.http.Request;
 import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Param;
 
+import javax.swing.text.Segment;
 import java.util.ArrayList;
 import java.util.List;
 
 @At("/segments")
 public class SegmentAction
 {
+  private static Logger logger = Logger.getLogger(Segment.class);
   private static JiebaSegmenter segmenter = new JiebaSegmenter();
 
   @At("/?")
   public ApiResult<String> get(String text)
   {
+    return segments(text);
+  }
+
+  @At("/")
+  public ApiResult<String> getV2(@Param("text") String text)
+  {
+    return segments(text);
+  }
+
+  private ApiResult<String> segments(String text){
+    if(text == null) text = "";
     List<SegToken> segTokenList = segmenter.process(text, JiebaSegmenter.SegMode.SEARCH);
     List<String> data = new ArrayList<>();
     for (SegToken segToken : segTokenList) {
